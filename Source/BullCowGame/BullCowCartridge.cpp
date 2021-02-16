ca23dd43aab9155a256de73dfd27fdc00f00fe79
ejdelrio <Eddie@Edwins-MacBook-Pro-2.local>
2021-02-15 23:00:40 -0800

    Removed duplicate files. Added additional logfic to generate random words for game

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,39 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+#define FSTRING class FString
+#define NUMBER_OF_WORDS 10
+
+FSTRING RandomWord;
+const FSTRING WelcomeMessage = TEXT("Welcome To Bull Cow");
+const FSTRING ContinuationPrompt = TEXT("Press enter to continue\n");
+const FSTRING HiddenWord = TEXT("Bacon");
+const FSTRING HiddenWordArray[NUMBER_OF_WORDS] =
+        {
+            "Bacon",
+            "Sauce",
+            "Money",
+            "Meat",
+            "Turkey",
+            "Sandwich",
+            "Women",
+            "Tower",
+            "Country",
+            "Pickels"
+        };
+
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
+    int indexOfRandomWordToGuess = rand() % NUMBER_OF_WORDS;
+    RandomWord = HiddenWordArray[indexOfRandomWordToGuess];
+
     Super::BeginPlay();
+    PrintLine(WelcomeMessage);
+    PrintLine(ContinuationPrompt);
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FSTRING& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    PrintLine(ContinuationPrompt);
 }
